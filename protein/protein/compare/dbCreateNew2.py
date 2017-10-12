877657b460015f73a589def29efeb2563f40800b
Titli Sarkar <titli010203@gmail.com>
2017-10-12 16:50:29 -0500

    Add files via upload

--- protein/protein/compare/dbCreateNew2.py
+++ protein/protein/compare/dbCreateNew2.py
@@ -5,6 +5,8 @@ import pandas as pd
 import csv
 import glob
 
+#path = 'E:\\Codes\\Python_Code\\Database Part\\'
+path = '//home//linc//txs7980//Desktop//Database Part//'
 # Open database connection
 db = MySQLdb.connect("localhost", "root", "123456", "ProteinDB")
 
@@ -20,70 +22,72 @@ print("Database version : %s " % data)
 
 #Insert Value in tables
 def insertCLASS_DESCRIPTION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\protein_hierarchyS2.csv', 'r') as f:
+    with open(path+'protein_hierarchyS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             cathinfo = row[1].split(".")
             Class = int(cathinfo[0])
             DescriptionOfClass = row[2]
-            cursor.execute ("SELECT * FROM COMPARE_CLASS_DESCRIPTION WHERE Class = %s", Class)
+            cursor.execute ("SELECT * FROM compare_class_description WHERE Class = %s", [Class])
             data = cursor.fetchall()
             if len(data) == 0:
                 try:
-                    cursor.execute("INSERT INTO COMPARE_CLASS_DESCRIPTION (Class,DescriptionOfClass) VALUES (%s,%s)",
+                    cursor.execute("INSERT INTO compare_class_description (Class,DescriptionOfClass) VALUES (%s,%s)",
                                    (Class, DescriptionOfClass))
                 except MySQLdb.Error, e:
                     print(e)
 def insertARCHITECTURE_DESCRIPTION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\protein_hierarchyS2.csv', 'r') as f:
+    with open(path+'protein_hierarchyS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             cathinfo = row[1].split(".")
             Architecture = int(cathinfo[1])
             DescriptionOfArchitecture = row[3]
-            cursor.execute("SELECT * FROM COMPARE_ARCHITECTURE_DESCRIPTION WHERE Architecture = %s", Architecture)
+            cursor.execute("SELECT * FROM compare_architecture_description WHERE Architecture = %s", [Architecture])
             data = cursor.fetchall()
             if len(data) == 0:
                 try:
                     cursor.execute(
-                        "INSERT INTO COMPARE_ARCHITECTURE_DESCRIPTION (Architecture,DescriptionOfArchitecture) VALUES (%s,%s)",
+                        "INSERT INTO compare_architecture_description (Architecture,DescriptionOfArchitecture) VALUES (%s,%s)",
                         (Architecture, DescriptionOfArchitecture))
                 except MySQLdb.Error, e:
                     print(e)
 def insertTOPOLOGYFOLD_DESCRIPTION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\protein_hierarchyS2.csv', 'r') as f:
+    with open(path+'protein_hierarchyS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             cathinfo = row[1].split(".")
             TopologyFold = int(cathinfo[2])
             DescriptionOfTopologyFold = row[4]
-            cursor.execute("SELECT * FROM COMPARE_TOPOLOGYFOLD_DESCRIPTION WHERE TopologyFold = %s", TopologyFold)
+            cursor.execute("SELECT * FROM compare_topologyfold_description WHERE TopologyFold = %s", [TopologyFold])
             data = cursor.fetchall()
             if len(data) == 0:
                 try:
                     cursor.execute(
-                        "INSERT INTO COMPARE_TOPOLOGYFOLD_DESCRIPTION (TopologyFold,DescriptionOfTopologyFold) VALUES (%s,%s)",
+                        "INSERT INTO compare_topologyfold_description (TopologyFold,DescriptionOfTopologyFold) VALUES (%s,%s)",
                         (TopologyFold, DescriptionOfTopologyFold))
                 except MySQLdb.Error, e:
                     print(e)
+
 def insertHOMOLOGYSUPERFAMILY_DESCRIPTION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\protein_hierarchyS2.csv', 'r') as f:
+    with open(path+'protein_hierarchyS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             cathinfo = row[1].split(".")
             HomologySuperfamily = int(cathinfo[3])
             DescriptionOfHomologySuperfamily = row[4]
-            cursor.execute("SELECT * FROM COMPARE_HOMOLOGYSUPERFAMILY_DESCRIPTION WHERE HomologySuperfamily = %s", HomologySuperfamily)
+            cursor.execute("SELECT * FROM compare_homologysuperfamily_description WHERE HomologySuperfamily = %s", [HomologySuperfamily])
             data = cursor.fetchall()
             if len(data) == 0:
                 try:
                     cursor.execute(
-                        "INSERT INTO COMPARE_HOMOLOGYSUPERFAMILY_DESCRIPTION (HomologySuperfamily,DescriptionOfHomologySuperfamily) VALUES (%s,%s)",
+                        "INSERT INTO compare_homologysuperfamily_description (HomologySuperfamily,DescriptionOfHomologySuperfamily) VALUES (%s,%s)",
                         (HomologySuperfamily, DescriptionOfHomologySuperfamily))
                 except MySQLdb.Error, e:
                     print(e)
+
 def insertPROTEIN_HIERARCHY():
-    with open('E:\\Codes\\Python_Code\\Database Part\\protein_hierarchyS2.csv', 'r') as f:
+    with open(path+'protein_hierarchyS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             Protein_ID = row[0]
@@ -94,15 +98,15 @@ def insertPROTEIN_HIERARCHY():
             TopologyFold = int(cathinfo[2])
             HomologySuperfamily = int(cathinfo[3])
             try:
-                cursor.execute("INSERT INTO COMPARE_PROTEIN_HIERARCHY"
+                cursor.execute("INSERT INTO compare_protein_hierarchy"
                                "(Protein_ID, Class_id, Architecture_id, TopologyFold_id, HomologySuperfamily_id) "
                                "VALUES (%s,%s,%s,%s,%s)",
-                               (Protein_ID, Class, Architecture, TopologyFold, HomologySuperfamily)
+                               ([Protein_ID], [Class], [Architecture], [TopologyFold], [HomologySuperfamily])
                                )
             except MySQLdb.Error, e:
                 print(e)
-def insertALL_PROTEINStest():   ## saving everything in a csv file and reading from it
-    with open("E:\\Codes\\Python_Code\\Database Part\\"+"all_proteinsS2test.csv", 'r') as f:  ## inserting in smaller test table
+def insertALL_PROTEINS():   ## saving everything in a csv file and reading from it
+    with open(path+"all_proteinsS2.csv", 'r') as f:  ## inserting in smaller test table
         reader = csv.reader(f)
         for row in reader:
             Protein_ID = str(row[0])
@@ -130,34 +134,33 @@ def insertALL_PROTEINStest():   ## saving everything in a csv file and reading f
             #print (Protein_ID, Protein_Key, Key_coourence_no, aacd0, position0, aacd1, position1, aacd2, position2, classT1, Theta, classL1, maxDist, x0, y0, z0, x1, y1, z1, x2, y2, z2)
             try:
                 cursor.execute(
-                    "INSERT INTO COMPARE_ALL_PROTEINSTEST(Protein_ID, Protein_Key, Key_coourence_no, aacd0, position0, aacd1, position1, aacd2, position2, classT1, Theta, classL1, maxDist, x0, y0, z0, x1, y1, z1, x2, y2, z2) "
+                    "INSERT INTO compare_all_proteinstest(Protein_ID, Protein_Key, Key_coourence_no, aacd0, position0, aacd1, position1, aacd2, position2, classT1, Theta, classL1, maxDist, x0, y0, z0, x1, y1, z1, x2, y2, z2) "
                     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                    (Protein_ID, Protein_Key, Key_coourence_no, aacd0, position0, aacd1, position1, aacd2, position2, classT1, Theta, classL1, maxDist, x0, y0, z0, x1, y1, z1, x2, y2, z2)
+                    ([Protein_ID], [Protein_Key], [Key_coourence_no], [aacd0], [position0], [aacd1], [position1], [aacd2], [position2], [classT1], [Theta], [classL1], [maxDist], [x0], [y0], [z0], [x1], [y1], [z1], [x2], [y2], [z2])
                 )
             except MySQLdb.Error, e:
                 print(e)
 def insertPOSITION_INFORMATION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\position_informationS2.csv', 'r') as f:
+    with open(path+'position_informationS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             fname = row[0]
             aminoAcidName = row[1]
-            seqid = str([row[2]])
-            #print seqid
+            seqid = int(row[2])
+            #print(type(seqid))
             xCord = float(row[3])
             yCord = float(row[4])
             zCord = float(row[5])
-            #print type(xCord)
             try:
                 cursor.execute(
-                        "INSERT INTO COMPARE_POSITION_INFORMATION(Protein_ID_id, AminoAcid_Name, Seq_ID, X_COORD, Y_COORD, Z_COORD) "
+                        "INSERT INTO compare_position_information(Protein_ID_id, AminoAcid_Name, Seq_ID, X_COORD, Y_COORD, Z_COORD) "
                         "VALUES (%s,%s,%s,%s,%s,%s)",
-                        (fname, aminoAcidName, seqid[0], xCord, yCord, zCord)
+                        ([fname], [aminoAcidName], [seqid], [xCord], [yCord], [zCord])
                 )
             except MySQLdb.Error, e:
                 print(e)
 def insertSIMILARITY_INFORMATION():
-    with open('E:\\Codes\\Python_Code\\Database Part\\similarity_informationS2.csv', 'r') as f:
+    with open(path+'similarity_informationS2.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
             Protein_Id1 = row[0]
@@ -165,23 +168,22 @@ def insertSIMILARITY_INFORMATION():
             Sim = row[2]
             #print (Protein_Id1,Protein_Id2,Sim)
             try:
-                cursor.execute("INSERT INTO COMPARE_SIMILARITY_INFORMATION(Protein_ID1_id,Protein_ID2_id,Similarity_Value) VALUES (%s,%s,%s)",
-                (Protein_Id1,Protein_Id2,Sim)
+                cursor.execute("INSERT INTO compare_similarity_information(Protein_ID1_id,Protein_ID2_id,Similarity_Value) VALUES (%s,%s,%s)",
+                ([Protein_Id1],[Protein_Id2],[Sim])
                 )
 
             except MySQLdb.Error, e:
                 print(e)
 
 #Function Calling
-insertCLASS_DESCRIPTION()
-insertARCHITECTURE_DESCRIPTION()
-insertTOPOLOGYFOLD_DESCRIPTION()
-insertHOMOLOGYSUPERFAMILY_DESCRIPTION()
-insertPROTEIN_HIERARCHY()
-#insertALL_PROTEINS()            ## !!!!!!!!!!!!
-insertPOSITION_INFORMATION() ######
-insertSIMILARITY_INFORMATION()
-insertALL_PROTEINStest() ## !!!!!!!!!!!!
+#insertCLASS_DESCRIPTION()
+#insertARCHITECTURE_DESCRIPTION()
+#insertTOPOLOGYFOLD_DESCRIPTION()
+#insertHOMOLOGYSUPERFAMILY_DESCRIPTION()
+#insertPROTEIN_HIERARCHY()
+insertALL_PROTEINS()            ## !!!!!!!!!!!!
+#insertPOSITION_INFORMATION() ######
+#insertSIMILARITY_INFORMATION()
 
 db.commit()
 db.close()
