a02caee268afdb1452629ff7918cd2de29cad2d4
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2017-11-04 16:33:42 -0500

    Completed the search by protein and protein and seq

--- protein/protein/search/views.py
+++ protein/protein/search/views.py
@@ -18,35 +18,38 @@ class SearchByProteinID(ListView):
 def display_3(request):
     """
     Input: A set of protein ids
-    Output: Commom keys, by clicking on each keys, show list of all protein_ids
-    having it and other details too
+    Output: Commom keys, by clicking on each keys, displays
+    list of all protein_ids having it and other details too
     """
     context = {}
-    s = request.POST.getlist("list3")
-    context['s'] = s
-    qs = ALL_PROTEINS.objects.filter(Protein_ID_id__in=s) \
+    protein_keys_list = []
+    protein_keys_dict = {}
+    user_protein_list = request.POST.getlist("list3")
+    context['protein_list'] = user_protein_list
+
+    protein_key_queryset = ALL_PROTEINS.objects.filter(
+        Protein_ID_id__in=user_protein_list)\
         .distinct()\
         .values('Protein_Key')\
-        .annotate(key_count=Count('Protein_Key')) \
-        .filter(key_count__gte=len(s)) \
+        .annotate(key_count=Count('Protein_Key'))\
+        .filter(key_count__gte=len(user_protein_list))\
         .order_by('Protein_Key')
 
-    db_rows = []
-    for dict in qs:
-        db_rows.append(dict.get('Protein_Key'))
-    context['i'] = db_rows
-    keys_dict = {}
-    for key in db_rows:
-        qs_desc_list = []
-        for prot in s:
-            qs_desc_list.append(ALL_PROTEINS.objects.filter(
-                Q(Protein_Key=str(key)) &
-                Q(Protein_ID_id=str(prot)))
-                .order_by('Protein_ID_id', 'Key_coourence_no'))
-        keys_dict[str(key)] = qs_desc_list
+    for query in protein_key_queryset:
+        protein_keys_list.append(query.get('Protein_Key'))
 
-    context['j'] = keys_dict
-    print(context['j'])
+    for key in protein_keys_list:
+        qs_desc_list = []
+        for prot in user_protein_list:
+            qs_desc_list.append(ALL_PROTEINS.objects.filter(
+                Q(Protein_Key=str(key))
+                & Q(Protein_ID_id=str(prot)))
+                .order_by('Protein_ID_id', 'Key_coourence_no'))
+        protein_keys_dict[str(key)] = qs_desc_list
+
+    context['protein_keys_list'] = protein_keys_list
+    context['protein_keys_dict'] = protein_keys_dict
+
     return render(request, 'response1.html', context)
 
 
@@ -60,26 +63,45 @@ def display_4_step1(request):
     if request.method == 'POST':
         pid = request.POST.get("list4")
         seq_id_queryset = POSITION_INFORMATION.objects.filter(
-            Q(Protein_ID=str(pid))).values('Seq_ID')
-        context['i'] = seq_id_queryset
+            Q(Protein_ID=str(pid)))\
+            .values('Seq_ID')
+        context['seq_list'] = seq_id_queryset
+
     return render(request, 'choice4_1.html', context)
 
 
 def display_4_step2(request):
     context = {}
     if request.method == 'POST':
+        protein_key_list = []
+        pid_list = []
+        protein_keys_dict = {}
         seq_list = request.POST.getlist("list4_1")
         context['seq_list'] = seq_list
-        qs = POSITION_INFORMATION.objects.filter(Seq_ID__in=seq_list)\
+
+        pos_info_queryset = POSITION_INFORMATION.objects.filter(
+            Seq_ID__in=seq_list)\
             .distinct().values('Protein_ID').order_by('Protein_ID')
-        pid_list = []
-        for dict in qs:
+
+        for dict in pos_info_queryset:
             pid_list.append(dict.get('Protein_ID'))
 
-        qs = ALL_PROTEINS.objects.filter(Protein_ID_id__in=pid_list)\
-            .values('Protein_ID_id', 'Protein_Key')\
+        all_proteins_queryset = ALL_PROTEINS.objects.filter(
+            Protein_ID_id__in=pid_list)\
+            .distinct()\
+            .values('Protein_Key')\
             .order_by('Protein_Key')\
             .distinct()
-        context['i'] = qs
-        print(context['i'])
+
+        for dict in all_proteins_queryset:
+            protein_key_list.append(dict.get('Protein_Key'))
+
+        for key in protein_key_list:
+            protein_keys_dict[str(key)] = ALL_PROTEINS.objects.filter(
+                Q(Protein_Key=str(key)))\
+                .order_by('Protein_ID_id', 'Key_coourence_no')
+
+        context['protein_keys_list'] = protein_key_list
+        context['protein_keys_dict'] = protein_keys_dict
+
     return render(request, 'response2.html', context)
