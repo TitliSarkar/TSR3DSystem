e084606cabed3d9638179403a248177bb5d3856f
Titli Sarkar <titli010203@gmail.com>
2017-12-05 15:54:31 -0600

    Fixed the search by sequence ID logical mistake

--- protein/protein/search/views.py
+++ protein/protein/search/views.py
@@ -12,6 +12,7 @@ from compare.models import PROTEIN_HIERARCHY
 
 import time
 from collections import Counter
+from itertools import combinations
 
 
 class SearchByProteinID(ListView):
@@ -31,7 +32,7 @@ def search_by_protein_id(request):
     user_protein_list = request.POST.getlist("list3")
 
     if not user_protein_list and "small_table" in request.POST:
-        user_protein_list = ['1a06', '1cdk', '1muo', '2src']
+        user_protein_list = ['1a06', '1muo']
 
     context['protein_list'] = user_protein_list
 
@@ -58,19 +59,22 @@ def search_by_protein_id(request):
     sub_query = all_proteins_table.objects.filter(
         Protein_Key__in=protein_key_list)\
         .distinct()\
-        .values_list('Protein_ID', 'Protein_Key')
+        .values_list('Protein_ID_id', 'Protein_Key')
 
     sub_query_list = [entry for entry in sub_query]
-
     d = {}
     cnt = Counter(elem[0] for elem in sub_query_list)
     for key, value in cnt.items():
         d[key] = value
 
     pro_list = filter(lambda x: x[1] >= len(protein_key_list), d.items())
+
     proteins = []
     for i in range(len(pro_list)):
         proteins.append(pro_list[i][0])
+
+    if not pro_list:
+        proteins = user_protein_list
 
     context['common_keys'] = protein_key_list
     context['proteins'] = proteins
@@ -104,17 +108,11 @@ def search_by_protein_id_seq_step2(request):
     context = {}
     if request.method == 'POST':
         protein_key_list = []
-        pid_list = []
         seq_list = request.POST.getlist("list4_1")
-        context['pid'] = request.POST.get('pid', '1a06')
+        pid = request.POST.get('pid', '1a06')
+        context['pid'] = pid
         context['seq_list'] = seq_list
-
-        pos_info_queryset = POSITION_INFORMATION.objects.filter(
-            Seq_ID__in=seq_list)\
-            .distinct().values('Protein_ID').order_by('Protein_ID')
-
-        for dict in pos_info_queryset:
-            pid_list.append(dict.get('Protein_ID'))
+        pos_list = list(combinations(seq_list, 3))
 
         if "small_table" in request.POST:
             all_proteins_table = ALL_PROTEINS
@@ -125,29 +123,43 @@ def search_by_protein_id_seq_step2(request):
         else:
             all_proteins_table = ALL_PROTEINS
 
-        all_proteins_queryset = all_proteins_table.objects.filter(
-            Protein_ID_id__in=pid_list)\
-            .distinct()\
-            .values('Protein_Key')\
-            .order_by('Protein_Key')\
-            .distinct()
+        pid_rows = all_proteins_table.objects.filter(Protein_ID_id=pid)
 
-        for dict in all_proteins_queryset:
-            protein_key_list.append(int(dict.get('Protein_Key')))
+        key_list = []
+        for positions in pos_list:
+            protein_keys = pid_rows.filter(
+                Q(position0__in=list(positions))
+                & Q(position1__in=list(positions))
+                & Q(position2__in=list(positions)))\
+                .distinct()\
+                .values('Protein_Key')
 
-        sub_query = all_proteins_table.objects.filter(
-            Protein_Key__in=protein_key_list)\
-            .distinct()\
-            .values_list('Protein_ID', 'Protein_Key')
+            for key in protein_keys:
+                key_list.append(key['Protein_Key'])
 
-        sub_query_list = [entry for entry in sub_query]
+        protein_key_list = set(key_list)
+
+        sub_query = []
+        for key in protein_key_list:
+            row_val = all_proteins_table.objects.filter(
+                Protein_Key=str(key))\
+                .distinct()\
+                .values('Protein_ID_id')\
+                .order_by('Protein_ID_id')
+            sub_query.append(row_val)
+
+        sub_query_list = []
+        for queryset in sub_query:
+            for l in queryset:
+                sub_query_list.append(str(l.get('Protein_ID_id')))
 
         d = {}
-        cnt = Counter(elem[0] for elem in sub_query_list)
+        cnt = Counter(elem for elem in sub_query_list)
         for key, value in cnt.items():
             d[key] = value
 
         pro_list = filter(lambda x: x[1] >= len(protein_key_list), d.items())
+
         proteins = []
         for i in range(len(pro_list)):
             proteins.append(pro_list[i][0])
